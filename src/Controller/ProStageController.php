f5e9511b690fbb21e87c4ff316224e0de2023485
Thomas Lapeyre <tlbady@iutbayonne.univ-pau.fr>
2019-11-21 09:37:17 +0100

    Modification du template base.html.twig

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -12,8 +12,32 @@ class ProstageController extends AbstractController
      */
     public function index()
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'ProstageController',
+        return $this->render('prostage/index.html.twig');
+    }
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function entreprises()
+    {
+        return $this->render('prostage/entreprises.html.twig');
+    }
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function formations()
+    {
+        return $this->render('prostage/formations.html.twig');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="stages")
+     */
+    public function stages(int $id)
+    {
+        return $this->render('prostage/stages.html.twig', [
+            'id_stage' => $id,
         ]);
     }
 }
