7f44bafecdde5f734aabaea809f642a30c664f1d
Thomas Lapeyre <tlbady@iutbayonne.univ-pau.fr>
2019-12-20 14:50:29 +0100

    Utilisation du mécanisme d'injection de dépendances dans ProstageController

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,15 +9,19 @@ use App\Entity\Stage;
 use App\Entity\Entreprise;
 use App\Entity\Formation;
 
+use App\Repository\StageRepository;
+use App\Repository\EntrepriseRepository;
+use App\Repository\FormationRepository;
+
 
 class ProstageController extends AbstractController
 {
     /**
      * @Route("/", name="prostage")
      */
-    public function index()
+    public function index(StageRepository $repositoryStage) // Utilisation du mécanisme d'injection de dépendances
     {
-        $listeStages = $this->getDoctrine()->getRepository(Stage::class)->findAll();
+        $listeStages = $repositoryStage->findAll();
         
         return $this->render('prostage/index.html.twig', [
             "listeStages" => $listeStages
@@ -27,9 +31,9 @@ class ProstageController extends AbstractController
     /**
      * @Route("/entreprises", name="entreprises")
      */
-    public function entreprises()
+    public function entreprises(EntrepriseRepository $repositoryEntreprise) // Utilisation du mécanisme d'injection de dépendances
     {
-        $listeEntreprises = $this->getDoctrine()->getRepository(Entreprise::class)->findBy(array(), array('nom' => 'ASC'));
+        $listeEntreprises = $repositoryEntreprise->findBy(array(), array('nom' => 'ASC'));
         
         return $this->render('prostage/entreprises.html.twig', [
             "listeEntreprises" => $listeEntreprises
@@ -39,10 +43,10 @@ class ProstageController extends AbstractController
     /**
      * @Route("/entreprises/{id}", name="entreprise_stages")
      */
-    public function entreprise_stages($id)
+    public function entreprise_stages(Entreprise $entreprise, StageRepository $repositoryStage) // Utilisation du mécanisme d'injection de dépendances
+    // Symfony crée un objet Entreprise en utilisant l'objet dont l'id est passée en paramètre'
     {
-        $entreprise = $this->getDoctrine()->getRepository(Entreprise::class)->find($id);
-        $listeStages = $this->getDoctrine()->getRepository(Stage::class)->findByEntreprise($id);
+        $listeStages = $repositoryStage->findByEntreprise($entreprise);
         
         return $this->render('prostage/entreprise_stages.html.twig', [
             "entreprise" => $entreprise,
@@ -53,9 +57,9 @@ class ProstageController extends AbstractController
     /**
      * @Route("/formations", name="formations")
      */
-    public function formations()
+    public function formations(FormationRepository $repositoryFormation) // Utilisation du mécanisme d'injection de dépendances
     {
-        $listeFormations = $this->getDoctrine()->getRepository(Formation::class)->findBy(array(), array('type' => 'ASC'));
+        $listeFormations = $repositoryFormation->findBy(array(), array('type' => 'ASC'));
         
         return $this->render('prostage/formations.html.twig', [
             "listeFormations" => $listeFormations
@@ -65,11 +69,10 @@ class ProstageController extends AbstractController
     /**
      * @Route("/formations/{id}", name="formation_stages")
      */
-    public function formation_stages($id)
+    public function formation_stages(Formation $formation, StageRepository $repositoryStage) // Utilisation du mécanisme d'injection de dépendances
+    // Symfony crée un objet Formation en utilisant l'objet dont l'id est passée en paramètre'
     {
-        $formation = $this->getDoctrine()->getRepository(Formation::class)->find($id);
-
-        $listeStages = $this->getDoctrine()->getRepository(Stage::class)->findById($formation->getStages()->getValues());
+        $listeStages = $repositoryStage->findById($formation->getStages()->getValues());
         
         return $this->render('prostage/formation_stages.html.twig', [
             "formation" => $formation,
@@ -80,9 +83,9 @@ class ProstageController extends AbstractController
     /**
      * @Route("/stages/{id}", name="stages")
      */
-    public function stages(int $id)
+    public function stages(Stage $stage) // Utilisation du mécanisme d'injection de dépendances
+    // Symfony crée un objet Stage en utilisant l'objet dont l'id est passée en paramètre'
     {
-        $stage = $this->getDoctrine()->getRepository(Stage::class)->find($id);
         return $this->render('prostage/stages.html.twig', [
             'stage' => $stage
         ]);
